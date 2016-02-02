0e3fedce6671ddd4f10d3d4c0e5b70784340f0fe
Alyson <emerald.aly@gmail.com>
2016-02-02 17:29:18 -0500

    added text to README.md and index.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Zoo_App
+
+I had MAMP installed on my computer so wasn't able to access MYSQL until Albert successfully removed evreryhthing and reloaded on Saturday morning.
+
+I had trouble importing the zoodb.
+
+I'm going to continue working on the assignment during the office hours, but for now am submitting what I've done so far.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//HW now complete. See README.md for notes
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -6,5 +8,79 @@ var connection = mysql.createConnection({
     database: 'zoo_db'
 });
 
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    };
+    console.log('connected as id ' + connection.threadId);
+});
 
-connection.connect();
+var prompt = require('prompt');
+prompt.start();
+prompt.message = "";
+
+var zoo = {
+    welcome: console.log("welcome to the Zoo and Friends App~!");
+    menu: console.log(Enter(A) to add an animal to the zoo '\n'
+        Enter(U) to update info on an animal '\n'
+        Enter(V) to visit the animals '\n'
+        Enter(D) to adopt an animal '\n'
+        Enter(Q) to exit);
+    add = function() {
+        return typeof input_scope[0]();
+        var currentScope = input_scope;
+        console.log("To add an animal to the zoo please fill out the following form for us");
+        prompt.get(['name', 'type', 'age'], function(err, result) {
+                connection.query(),
+                    function(err, rows, fields) {
+                        if (err) throw err;
+
+                        for (var i = 0; i < rows.length; i++) {
+                            console.log(rows[i].name + " is " + rows[i].this + ".");
+                            // return start menu
+                        }
+                    });
+        visit = function() {
+            console.log('Enter (I) Do you know the animal by ID? We will visit that animal');
+            console.log('Enter (N) Do you know the animal by name? We will visit that animal');
+            console.log('Enter (A) Here is the count of the animals at all locations');
+            console.log('Enter (C) Here is the count of all the animals in a city');
+            console.log('Enter (O) Here is the count for all the animals in all locations by type');
+            console.log('Enter (Q) Quit to the main menu');
+            currentScope.visit();
+            currentScope.view(currentScope)();
+
+        view = function()   {
+            return typeof input_scope[0]();
+            var currentScope = input_scope;
+            console.log("Choose what you would like to visit");
+            prompt.get(['visit'], function(err, rows, fields) {
+            if (err) throw err;)
+            if result.visit === 'Q'
+                currentScope.menu();
+            else if result.visit === "O"
+                currentScope.type(input_scope);
+            else if result.type === "I"
+                currentScope.type(input_scope);
+            else if result.animId === "N"
+                currentScope.name(input_scope);
+            else if result.name === "A"
+                current.Scope.all(input_scope);
+            else if result.all === "C"
+                currentScope.care(input_scope);
+            else
+                console.log("Sorry didn't get that, come again?");
+            currentScope.visit();
+            currentScope.view(currentScope);
+
+            type: 
+
+        }  
+
+    }    
+        });
+currentScope.menu();
+currentScope.promptUser();
+}
+
